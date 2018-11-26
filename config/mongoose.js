0b4e20bb3a1a1831210bf65e0e3be1b876c90ea7
Deepak <raj.deeepak@gmail.com>
2018-11-27 03:50:03 +0530

    deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/A2Z-app');
+const URI = "mongodb://a2z-db:CtWQvUn0MLbor5L6NVxenPjrdcs54C43Ih4ihCCjlHrx5H6YV3aVMr2OUQh6SuCmZ6E8X42Ns8zrv3uRsIlYVw==@a2z-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+mongoose.connect(URI);
 
 module.exports = {mongoose};
