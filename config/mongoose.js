8d659da1cb2943d5cc3aa604e6eea3de9b596470
Deepak <raj.deeepak@gmail.com>
2018-11-27 23:58:50 +0530

    db host fixed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://a2z-db:CtWQvUn0MLbor5L6NVxenPjrdcs54C43Ih4ihCCjlHrx5H6YV3aVMr2OUQh6SuCmZ6E8X42Ns8zrv3uRsIlYVw%3D%3D@a2z-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+const URI = "mongodb+srv://dipakkra2z:adminadmin@a2z-db-qtqe6.azure.mongodb.net/A2Z-DB";
 
 mongoose.connect(URI).then(()=>{
     console.log('Connected to DB')
