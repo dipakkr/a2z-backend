2887dfbbe3efeb3b6d0ce77237b5d18dbd38c302
Deepak <raj.deeepak@gmail.com>
2018-11-27 17:58:36 +0530

    deployment update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-const URI = "mongodb://a2z-db:CtWQvUn0MLbor5L6NVxenPjrdcs54C43Ih4ihCCjlHrx5H6YV3aVMr2OUQh6SuCmZ6E8X42Ns8zrv3uRsIlYVw==@a2z-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
-mongoose.connect(URI);
+const URI = "mongodb://a2z-db:CtWQvUn0MLbor5L6NVxenPjrdcs54C43Ih4ihCCjlHrx5H6YV3aVMr2OUQh6SuCmZ6E8X42Ns8zrv3uRsIlYVw%3D%3D@a2z-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+
+mongoose.connect(URI).then(()=>{
+    console.log('Connected to DB')
+}).catch((err)=>{
+    console.err(err);
+});
 
 module.exports = {mongoose};
